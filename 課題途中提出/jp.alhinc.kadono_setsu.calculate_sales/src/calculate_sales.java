fc4068e63ac8764371036dbe618f810680bf4fa8
euthtari <euthserh@gmail.com>
2017-04-17 14:04:15 +0900

    メソッドの導入と動作確認をしました。

--- 課題途中提出/jp.alhinc.kadono_setsu.calculate_sales/src/calculate_sales.java
+++ 課題途中提出/jp.alhinc.kadono_setsu.calculate_sales/src/calculate_sales.java
@@ -30,53 +30,13 @@ public class calculate_sales {
 		// 支店定義ファイル読み込み一時リスト作成
 
 		// 支店定義ファイル読み込みスタート
-
-
-		if(fileRead(args[0],"branch",branchOneTime,branchDateMap,branchSalesDateMap)){
+		if(fileRead(args[0],"branch","支店",branchOneTime,branchDateMap,branchSalesDateMap,"^[0-9]{3}$")){
 		}else{
 			return;
-
 		}
-
-		/*		try {
-			File file = new File(args[0], "branch.lst");
-			fr = new FileReader(file);
-			br = new BufferedReader(fr);
-
-			String s;
-			while ((s = br.readLine()) != null) {
-				branchOneTime.add(s);
-			}
-		} catch (IOException e) {
-			System.out.println("支店定義ファイルが存在しません");
-			System.exit(1);
-		} finally {
-			try {
-				br.close();
-			} catch (IOException e) {
-				// TODO 自動生成された catch ブロック
-				e.printStackTrace();
-			}
-		}
-
-		// 格納後の支店定義ファイルをマップへ
-		for (int i = 0; i < branchOneTime.size(); i++) {
-			String bot = branchOneTime.get(i);
-
-			String[] branchDateSplit = bot.split(",");
-
-			if (branchDateSplit.length == 2 && branchDateSplit[1].matches("^[^\n\r]+$")
-					&& branchDateSplit[0].matches("^[0-9]{3}$")) {
-				branchDateMap.put(branchDateSplit[0], branchDateSplit[1]);
-				branchSalesDateMap.put(branchDateSplit[0], 0l);
-			} else {
-				System.out.println("支店定義ファイルのフォーマットが不正です");
-				System.exit(1);
-			}
-		}
-
 		// 支店定義ファイル読み込み終了
-		 * */
+
+
 
 		HashMap<String, String> commodityDateMap = new HashMap<String, String>();
 		// 商品定義マップ作成
@@ -87,50 +47,12 @@ public class calculate_sales {
 		ArrayList<String> commodityOneTime = new ArrayList<String>();
 		// 商品定義ファイル一時保存リスト作成
 
-		if(fileRead(args[0],"commodity",commodityOneTime,commodityDateMap,commoditySalesDateMap)){
-			System.out.println("商品定義ファイルが存在しません");
-		}
 		// 商品定義ファイル読み込みスタート
-
-		/*		try {
-
-			File file = new File(args[0], "commodity.lst");
-			fr = new FileReader(file);
-			br = new BufferedReader(fr);
-
-			String s;
-			while ((s = br.readLine()) != null) {
-				commodityOneTime.add(s);
-			}
-		} catch (IOException e) {
-			System.out.println("商品定義ファイルが存在しません");
-			System.exit(1);
-		} finally {
-			try {
-				br.close();
-			} catch (IOException e) {
-				// TODO 自動生成された catch ブロック
-				e.printStackTrace();
-			}
+		if(fileRead(args[0],"commodity","商品",commodityOneTime,commodityDateMap,commoditySalesDateMap,"^[0-9a-zA-Z]{8}")){
+		}else{
+			return;
 		}
-
-		// 格納後の商品定義ファイルをマップへ
-
-		for (int i = 0; i < commodityOneTime.size(); i++) {
-			String cot = commodityOneTime.get(i);
-			String[] commodityDateSplit = cot.split(",");
-			if (commodityDateSplit.length == 2 && commodityDateSplit[1].matches("^[^\n\r]+$")
-					&& commodityDateSplit[0].matches("^[0-9a-zA-Z]{8}")) {
-				commodityDateMap.put(commodityDateSplit[0], commodityDateSplit[1]);
-				commoditySalesDateMap.put(commodityDateSplit[0], 0l);
-			} else {
-				System.out.println("商品定義ファイルのフォーマットが不正です");
-				System.exit(1);
-			}
-		}
-
 		// 商品定義ファイル処理終了
-		 */
 
 
 		ArrayList<String> salesName = new ArrayList<String>();
@@ -262,105 +184,91 @@ public class calculate_sales {
 				return;
 			}
 		}
-
 	}
 
-	public static boolean fileRead(String dirPath,String fileName, ArrayList<String> oneTime,HashMap<String,String> nameMap,HashMap<String,Long> salesMap){
+
+	public static boolean fileRead(String dirPath,String fileName, String fileType,ArrayList<String> oneTime,
+			HashMap<String,String> nameMap,HashMap<String,Long> salesMap,String nameLimit){
 
 		// 定義ファイル読み込みスタート
 		BufferedReader br = null;
-		try {
-			File file = new File(dirPath, fileName + ".lst");
-			FileReader fr = new FileReader(file);
-			br = new BufferedReader(fr);
-
-			String s;
-			while ((s = br.readLine()) != null) {
-				oneTime.add(s);
-			}
-		} catch (IOException e) {
-			if(fileName == "branch"){
-			System.out.println("支店定義ファイルが存在しません");
+		File file = new File(dirPath, fileName + ".lst");
+		if(file.exists() == false){
+			System.out.println(fileType +"定義ファイルが存在しません");
 			return false;
-			}else{
-			System.out.println("商品定義ファイルが存在しません");
-			return false;
-			}
-			
-		} finally {
+		}else{
 			try {
-				br.close();
+
+				FileReader fr = new FileReader(file);
+				br = new BufferedReader(fr);
+
+				String s;
+				while ((s = br.readLine()) != null) {
+					oneTime.add(s);
+				}
 			} catch (IOException e) {
+				return false;
+			} finally {
+				try {
+					br.close();
+				} catch (IOException e) {
 
+				}
 			}
-		}
 
-		// 格納後の定義ファイルをマップへ
-		for (int i = 0; i < oneTime.size(); i++) {
-			String bot = oneTime.get(i);
+			// 格納後の定義ファイルをマップへ
+			for (int i = 0; i < oneTime.size(); i++) {
+				String bot = oneTime.get(i);
 
-			String[] DateSplit = bot.split(",");
-			if(fileName == "branch"){
-				if (DateSplit.length == 2 && DateSplit[1].matches("^[^\n\r]+$")
-						&& DateSplit[0].matches("^[0-9]{3}$")) {
+				String[] DateSplit = bot.split(",");
+				if (DateSplit.length == 2 && DateSplit[0].matches(nameLimit)) {
 					nameMap.put(DateSplit[0], DateSplit[1]);
 					salesMap.put(DateSplit[0], 0l);
 				} else {
-					return false; //"支店定義フォーマット不正";
+					System.out.println(fileType + "定義ファイルのフォーマットが不正です");
+					return false;
 				}
-			}else{
-				if (DateSplit.length == 2 && DateSplit[1].matches("^[^\n\r]+$")
-						&& DateSplit[0].matches("^[0-9a-zA-Z]{8}")) {
-					nameMap.put(DateSplit[0], DateSplit[1]);
-					salesMap.put(DateSplit[0], 0l);
-				} else {
-					return false; //"商品定義フォーマット不正";
-				}
-				}
-				
 			}
-
-
-			// 定義ファイル読み込み終了
-
-
-			return true;
 		}
 
-
-
-		public static boolean fileWright(String dirPath, String fileName, HashMap<String, String> nameMap,
-				HashMap<String, Long> salesMap) {
-
-			// ここからソート処理
-
-			List<Map.Entry<String, Long>> fileNameEntries = new ArrayList<Map.Entry<String, Long>>(salesMap.entrySet());
-			Collections.sort(fileNameEntries, new Comparator<Map.Entry<String, Long>>() {
-
-				public int compare(Entry<String, Long> entry1, Entry<String, Long> entry2) {
-					return ((Long) entry2.getValue()).compareTo((Long) entry1.getValue());
-				}
-			});
-
-			// ここまでソート処理
-
-			// ここからファイル出力処理
-			try {
-				File file = new File(dirPath, fileName + ".out");
-				FileWriter fw = new FileWriter(file);
-				BufferedWriter bw = new BufferedWriter(fw);
-				for (Entry<String, Long> s : fileNameEntries) {
-					bw.write(s.getKey() + "," + nameMap.get(s.getKey()) + "," + s.getValue() + "\r\n");
-					// 出力内容
-				}
-
-				bw.close();
-			} catch (FileNotFoundException e) {
-				return false;
-			} catch (IOException e) {
-				return false;
-			}
-			// ここまでファイル出力処理
-			return true;
-		}
+		// 定義ファイル読み込み終了
+		return true;
 	}
+
+
+
+	public static boolean fileWright(String dirPath, String fileName, HashMap<String, String> nameMap,
+			HashMap<String, Long> salesMap) {
+
+		// ここからソート処理
+
+		List<Map.Entry<String, Long>> fileNameEntries = new ArrayList<Map.Entry<String, Long>>(salesMap.entrySet());
+		Collections.sort(fileNameEntries, new Comparator<Map.Entry<String, Long>>() {
+
+			public int compare(Entry<String, Long> entry1, Entry<String, Long> entry2) {
+				return ((Long) entry2.getValue()).compareTo((Long) entry1.getValue());
+			}
+		});
+
+		// ここまでソート処理
+
+		// ここからファイル出力処理
+		try {
+			File file = new File(dirPath, fileName + ".out");
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			for (Entry<String, Long> s : fileNameEntries) {
+				bw.write(s.getKey() + "," + nameMap.get(s.getKey()) + "," + s.getValue() + "\r\n");
+				// 出力内容
+			}
+
+			bw.close();
+		} catch (FileNotFoundException e) {
+			return false;
+		} catch (IOException e) {
+			return false;
+		}
+		// ここまでファイル出力処理
+		return true;
+	}
+}
